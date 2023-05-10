ebc3386324fdf064a124697afad0d2f9e63a82c5
Husni Kamal <husni@wit.id>
2023-05-10 19:13:52 +0700

    update vite config

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if (command !== "serve") {
+    // command === 'build'
+    return {
+      // build specific config
+      base: "/wano-maps",
+    };
+  }
+  return {};
+});
